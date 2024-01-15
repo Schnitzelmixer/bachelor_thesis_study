d0a5c098c3843649d11c1a1603009f158f3c1f43
Julian Dockendorf <julian.dockendorf@stud-mail.uni-wuerzburg.de>
2024-01-15 21:20:38 +0100

    Annotation hide logic; article opacity lower

--- Assets/Scripts/HighlightAndAnnotationManager.cs
+++ Assets/Scripts/HighlightAndAnnotationManager.cs
@@ -1,30 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class HighlightAndAnnotationManager : MonoBehaviour {
-    public GameObject HoverPanel;
+public class HighlightAndAnnotationManager : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject relatedAnnotation;
 
-    public GameObject OptionalSecondHighlight;
+    public GameObject optionalSecondHighlight;
 
-    private void Start()
+    private GameObject currentlyActiveAnnotation;
+
+    private Image image;
+
+    private Color defaultColor = new Color(1.0f, 1.0f, 0.0f, 1.0f);
+
+    private Color hoverColor = new Color(1.0f, 0.97f, 0.0f, 1.0f);
+
+    void Start()
     {
-		Button btn = this.GetComponent<Button>();
-		btn.onClick.AddListener(SetAnnotationActive);
+		this.image = this.GetComponent<Image>();
     }
 
-	private void SetAnnotationActive()
+    void Update()
     {
-		Debug.Log ("You have clicked the button!");
-        HoverPanel.SetActive(true);
+        if (OVRInput.Get(OVRInput.RawButton.LIndexTrigger) || OVRInput.Get(OVRInput.RawButton.RIndexTrigger))
+        {
+            this.currentlyActiveAnnotation.SetActive(false);
+        }
+    }
+
+	public void ActivateAnnotation()
+    {
+        relatedAnnotation.SetActive(true);
+        this.currentlyActiveAnnotation = relatedAnnotation;
 	}
 
-    private void SetOptionalSecondHighlightToHoverState()
+    public void ActivateHoverState()
     {
-        if (OptionalSecondHighlight != null)
+        image.color = hoverColor;
+        if (optionalSecondHighlight != null)
         {
-            Debug.Log("Hover");
+            optionalSecondHighlight.GetComponent<Image>().color = hoverColor;
         }        
+    }
+
+    public void DeactivateHoverState()
+    {
+        image.color = defaultColor;
+        if (optionalSecondHighlight != null)
+        {
+            optionalSecondHighlight.GetComponent<Image>().color = defaultColor;
+        }        
+    }
+
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        ActivateHoverState();
+    }
+
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        DeactivateHoverState();
     }
 }
